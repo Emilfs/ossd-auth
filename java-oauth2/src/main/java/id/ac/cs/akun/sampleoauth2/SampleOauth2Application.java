4caae9947228535fa2ee50cfd8b5c33eeda7576d
giovanism <giovanism@outlook.co.id>
2020-08-06 18:17:21 +0700

    Add java sample for oauth2 using authorization_code grant type

---
+++ java-oauth2/src/main/java/id/ac/cs/akun/sampleoauth2/SampleOauth2Application.java
@@ -0,0 +1,13 @@
+package id.ac.cs.akun.sampleoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleOauth2Application.class, args);
+	}
+
+}
