4c0a9b2be4997d51699ae99a23ed8c63cec97156
Giovan Isa Musthofa <giovan.isa@ui.ac.id>
2020-08-24 14:09:30 +0700

    Add django cas + oauth2 client sample

---
+++ django/web/backends.py
@@ -0,0 +1,29 @@
+from urllib.parse import urlencode
+from social_core.backends.oauth import BaseOAuth2
+
+
+class AkunOAuth2(BaseOAuth2):
+    """Akun OAuth2 authentication backend"""
+    name = 'akun'
+    AUTHORIZATION_URL = 'https://akun-kp.cs.ui.ac.id/cas/oauth2.0/authorize'
+    ACCESS_TOKEN_URL = 'https://akun-kp.cs.ui.ac.id/cas/oauth2.0/token'
+    ACCESS_TOKEN_METHOD = 'POST'
+    SCOPE_SEPARATOR = ','
+    EXTRA_DATA = [
+        ('attributes', 'attributes'),
+        ('expires_in', 'expires')
+    ]
+
+    def get_user_details(self, response):
+        """Return user details from Akun profile"""
+        print('backends.py:18', response)
+        return {
+            'username': response.get('id'),
+        }
+
+    def user_data(self, access_token, *args, **kwargs):
+        """Loads user data from service"""
+        url = 'https://akun-kp.cs.ui.ac.id/cas/oauth2.0/profile?' + urlencode({
+            'access_token': access_token
+        })
+        return self.get_json(url)
---
+++ django/web/views.py
@@ -0,0 +1,47 @@
+from django.conf import settings
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html', context={
+        'title': 'Index',
+    })
+
+
+def login(request):
+    context = {'title': "Login"}
+    if request.user.is_authenticated:
+        location = request.GET.get('next2') or settings.LOGIN_REDIRECT_URL
+        return redirect(location)
+    return render(request, 'login.html', context=context)
+
+
+@login_required(redirect_field_name='next2')
+def logout(request):
+    context = {'title': "Logout"}
+    return render(request, 'logout.html', context=context)
+
+
+@login_required(redirect_field_name='next2')
+def oauth2_logout(request):
+    auth.logout(request)
+    return redirect(settings.LOGOUT_REDIRECT_URL)
+
+
+@login_required(redirect_field_name='next2')
+def profile(request):
+    attributes = {}
+
+    if request.user.social_auth.exists():
+        attributes["oauth2"] = request.user.social_auth.get().extra_data
+
+    if "attributes" in request.session:
+        attributes["cas"] = request.session["attributes"]
+
+    return render(request, 'profile.html', context={
+        'title': 'Profile',
+        'attributes': attributes,
+    })
