e4b920cd52f4893b1e697cd9970168a39cdfc990
Jonathan Filbert <jonathan.lisyanto@bpkpenabur.sch.id>
2020-10-24 02:42:20 +0700

    logout issue

--- django/web/urls.py
+++ django/web/urls.py
@@ -11,9 +11,7 @@ urlpatterns = [
     path('login/cas/', django_cas_ng.views.LoginView.as_view(), name='cas_ng_login'),
     path('logout/cas/', django_cas_ng.views.LogoutView.as_view(),
          name='cas_ng_logout'),
-    path('logout/', logout, name="logout"),
-    path('logout/ouath2/', oauth2_logout, name='oauth2_logout'),
     path("profile/", profile, name='profile'),
-    path('/logout_success', logout_success, name="logout_success"),
+    path('logout_success/', logout_success, name="logout_success"),
     path("", index),
 ]
